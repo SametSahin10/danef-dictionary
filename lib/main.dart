a6abac984e865a300e5e4110de95a0bc5242d264
SametSahin10 <a.sahin.ual@gmail.com>
2019-12-10 23:32:25 +0300

    Showing suggestions on TextField using flutter_typeahead package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,30 +34,54 @@ class SearchFieldWidget extends StatefulWidget {
 }
 
 class _SearchFieldWidgetState extends State<SearchFieldWidget> {
+  var dummmyWords = ['aaaxxx', 'bbbxxxx', 'cccccxxxx', 'dddxxxx'];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16.0),
-      child: TextField(
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.blue,
-                width: 1.5,
+      child: TypeAheadField(
+        textFieldConfiguration: TextFieldConfiguration(
+            style: DefaultTextStyle.of(context).style.copyWith(fontSize: 18),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.blue,
+                  width: 1.5,
+                ),
+                borderRadius: new BorderRadius.circular(16.0),
               ),
-              borderRadius: new BorderRadius.circular(16.0)),
-          focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.blue,
-                width: 1.5,
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.blue,
+                  width: 1.5,
+                ),
+                borderRadius: new BorderRadius.circular(16.0),
               ),
-              borderRadius: new BorderRadius.circular(16.0)),
-          hintText: 'Search a word',
-          hintStyle: TextStyle(
-            fontSize: 16.0,
-          ),
-        ),
+            )),
+        suggestionsCallback: (pattern) {
+          return pattern.isEmpty ? null : _getSuggestions(pattern);
+        },
+        itemBuilder: (context, suggestion) {
+          return ListTile(
+            leading: Icon(Icons.beach_access),
+            title: Text(suggestion),
+          );
+        },
+        onSuggestionSelected: (suggestion) {
+          print(suggestion);
+        },
       ),
     );
+  }
+
+  List<String> _getSuggestions(String pattern) {
+    List<String> suggestionsToReturn = new List();
+    for (final word in dummmyWords) {
+      if (word.contains(pattern)) {
+        suggestionsToReturn.add(pattern);
+      }
+    }
+    return suggestionsToReturn;
   }
 }
