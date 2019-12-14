2c8b7be7225cecca5c6ff2ba81d6dd16a9f63963
SametSahin10 <a.sahin.ual@gmail.com>
2019-12-14 21:39:25 +0300

    Added annotations to word.dart and generated word.g.dart in order to get required methods

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -1,5 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'word.g.dart';
+
+@JsonSerializable()
 class Word {
   String adige;
   String turkish;
   Word(this.adige, this.turkish);
+
+  factory Word.fromJson(Map<String, dynamic> json) => _$WordFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WordToJson(this);
+}
+
+@JsonSerializable()
+class WordList {
+  List<Word> words;
+
+  WordList({this.words});
+
+  factory WordList.fromJson(List<dynamic> json) {
+    return WordList(
+      words: json
+          .map((e) => Word.fromJson(e as Map<String, dynamic>))
+          .toList());
+  }
 }
---
+++ lib/models/word.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'word.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Word _$WordFromJson(Map<String, dynamic> json) {
+  return Word(json['adige'] as String, json['turkish'] as String);
+}
+
+Map<String, dynamic> _$WordToJson(Word instance) =>
+    <String, dynamic>{'adige': instance.adige, 'turkish': instance.turkish};
+
+WordList _$WordListFromJson(Map<String, dynamic> json) {
+  return WordList(
+      words: (json['words'] as List)
+          ?.map((e) =>
+              e == null ? null : Word.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$WordListToJson(WordList instance) =>
+    <String, dynamic>{'words': instance.words};
