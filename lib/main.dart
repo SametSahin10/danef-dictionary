e9d212ec7c95adcfc4be282f6d79d8d31c2f2df9
SametSahin10 <a.sahin.ual@gmail.com>
2019-12-14 20:56:50 +0300

    Switched to using a different file for search field page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:danef_dictionary/screens/search_field_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,75 +13,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Danef Dictionary'),
         ),
-        body: HomePage(),
+        body: SearchFieldPage(),
       ),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SearchFieldWidget(),
-    );
-  }
-}
-
-class SearchFieldWidget extends StatefulWidget {
-  @override
-  _SearchFieldWidgetState createState() => _SearchFieldWidgetState();
-}
-
-class _SearchFieldWidgetState extends State<SearchFieldWidget> {
-  var dummmyWords = ['aaaxxx', 'bbbxxxx', 'cccccxxxx', 'dddxxxx'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: TypeAheadField(
-        textFieldConfiguration: TextFieldConfiguration(
-            style: DefaultTextStyle.of(context).style.copyWith(fontSize: 18),
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue,
-                  width: 1.5,
-                ),
-                borderRadius: new BorderRadius.circular(16.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue,
-                  width: 1.5,
-                ),
-                borderRadius: new BorderRadius.circular(16.0),
-              ),
-            )),
-        suggestionsCallback: (pattern) {
-          return pattern.isEmpty ? null : _getSuggestions(pattern);
-        },
-        itemBuilder: (context, suggestion) {
-          return ListTile(
-            leading: Icon(Icons.beach_access),
-            title: Text(suggestion),
-          );
-        },
-        onSuggestionSelected: (suggestion) {
-          print(suggestion);
-        },
-      ),
-    );
-  }
-
-  List<String> _getSuggestions(String pattern) {
-    List<String> suggestionsToReturn = new List();
-    for (final word in dummmyWords) {
-      if (word.contains(pattern)) {
-        suggestionsToReturn.add(pattern);
-      }
-    }
-    return suggestionsToReturn;
   }
 }
---
+++ lib/screens/search_field_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+class SearchFieldPage extends StatefulWidget {
+  @override
+  _SearchFieldPageState createState() => _SearchFieldPageState();
+}
+
+class _SearchFieldPageState extends State<SearchFieldPage> {
+  var dummmyWords = ['aaaxxx', 'bbbxxxx', 'cccccxxxx', 'dddxxxx'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: TypeAheadField(
+            textFieldConfiguration: TextFieldConfiguration(
+                textAlign: TextAlign.center,
+                style: DefaultTextStyle.of(context).style.copyWith(fontSize: 18),
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1.5,
+                      ),
+                      borderRadius: new BorderRadius.circular(24.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1.5,
+                      ),
+                      borderRadius: new BorderRadius.circular(16.0),
+                    ),
+                    hintStyle: TextStyle(fontSize: 18.0),
+                    hintText: 'Search a word')),
+            suggestionsCallback: (pattern) {
+              return pattern.isEmpty ? null : _getSuggestions(pattern);
+            },
+            itemBuilder: (context, suggestion) {
+              return ListTile(
+                leading: Icon(Icons.beach_access),
+                title: Text(suggestion),
+              );
+            },
+            onSuggestionSelected: (suggestion) {
+              print(suggestion);
+            },
+          )),
+    );
+  }
+
+  List<String> _getSuggestions(String pattern) {
+    List<String> suggestionsToReturn = new List();
+    for (final word in dummmyWords) {
+      if (word.contains(pattern)) {
+        suggestionsToReturn.add(word);
+      }
+    }
+    return suggestionsToReturn;
+  }
+}
